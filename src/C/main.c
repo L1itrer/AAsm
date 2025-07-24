28983c50791270b0be9e1bebcc55a5e72b254a68
Litrer <106829230+L1itrer@users.noreply.github.com>
2025-07-24 15:20:14 +0200

    Merge utility into main.c

--- src/C/main.c
+++ src/C/main.c
@@ -1,9 +1,280 @@
-#include "utility.h"
-
 const char msg[] = "Ready? Set! GO!\x0A";
-const u8 number[] = "1001";
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdbool.h>
+#ifdef _WIN32
+#else
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#endif
+#include <errno.h>
+#include <assert.h>
 
 
+// ---------------------------------------------------------
+// -----------------------UTILITY---------------------------
+// ---------------------------------------------------------
+
+
+#ifndef stdout
+#define stdout 1
+#define stderr 2
+#endif //stdout
+
+#define UNREACHABLE(msg) do { fprintf(stderr, "%s:%d UNREACHABLE: %s at", __FILE__, __LINE__, msg); abort(); } while(0)
+typedef unsigned char byte;
+typedef int8_t i8;
+typedef uint8_t u8;
+typedef int16_t i16;
+typedef uint16_t u16;
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef int64_t i64;
+typedef uint64_t u64;
+
+#define DA_INIT_CAP 1024*8
+
+typedef struct String
+{
+    char* data;
+    u64 count;
+    u64 capacity;
+} String;
+
+typedef struct StringView
+{
+    char* pointer;
+    u64 length;
+} StringView;
+
+typedef StringView SV;
+
+bool string_read_file(const char* path, String* str);
+
+
+//TODO: To rewrite this thing in itself I need my own implementations of libc functions
+// memory related functions:
+
+void* aasm_memset(void* buffer, i32 value, u64 count);
+void* aasm_memcpy(void* dst, void* src, u64 count);
+void* aasm_malloc(u64 count);
+void* aasm_realloc(void* buffer, u64 count);
+#define aasm_append(da, item)                                                          \
+    do {                                                                                 \
+        if ((da)->count >= (da)->capacity) {                                             \
+            (da)->capacity = (da)->capacity == 0 ? DA_INIT_CAP : (da)->capacity*2;   \
+            (da)->items = aasm_realloc((da)->items, (da)->capacity*sizeof(*(da)->items)); \
+            assert((da)->items != NULL && "Buy more RAM lol");                       \
+        }                                                                                \
+                                                                                         \
+        (da)->items[(da)->count++] = (item);                                             \
+    } while (0)
+
+
+// standard output
+
+typedef enum AasmLogLevel
+{
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR
+}AasmLogLevel;
+
+void aasm_log(AasmLogLevel level, const char* format, ...);
+
+// number conversion
+
+typedef enum Bases{
+    BASE_INVALID,
+    BASE_BIN = 2,
+    BASE_OCT = 8,
+    BASE_DEC = 10,
+    BASE_HEX = 16,
+}Bases;
+
+i32 sv_to_i32(SV slice, Bases base);
+u64 string_to_u64(SV slice, Bases base);
+bool i32_to_string(String* str, i32 number, Bases base);
+bool is_number(u8 c);
+Bases bases_from_char(char c);
+i32 sv_cmp(SV a, SV b);
+
+i32 sv_cmp_cstr(SV a, const char* b);
+
+
+
+bool string_read_file(const char* path, String* str)
+{
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) goto failure;
+    if (fseek(file, 0, SEEK_END) == -1) goto failure;
+    long file_size = ftell(file);
+    if (file_size < 0) goto failure;
+    if (fseek(file, 0, SEEK_SET) < 0) goto failure;
+
+    u64 new_count = str->count + file_size;
+    if (new_count > str->capacity)
+    {
+        str->capacity = new_count;
+        str->data = aasm_realloc(str->data, str->capacity);
+    }
+    fread(str->data + str->count, file_size, 1, file);
+    if (ferror(file)) goto failure;
+    str->count = new_count;
+    fclose(file);
+    return true;
+
+failure:
+    aasm_log(LOG_ERROR, "Could not read file %s: %s", path, strerror(errno));
+    fclose(file);
+    return false;
+}
+
+
+// memory related functions:
+
+void* aasm_memset(void* buffer, i32 value, u64 count)
+{
+    return memset(buffer, value, count);
+}
+
+void* aasm_memcpy(void* dst, void* src, u64 count)
+{
+    return memcpy(dst, src, count);
+}
+
+void* aasm_malloc(u64 count)
+{
+    return malloc(count);
+}
+
+void* aasm_realloc(void* buffer, u64 count)
+{
+    return realloc(buffer, count);
+}
+
+// standard output
+
+void aasm_log(AasmLogLevel level, const char* format, ...)
+{
+    switch(level)
+    {
+        case LOG_INFO:
+            fprintf(stdout, "[INFO]: ");
+            break;
+        case LOG_WARNING:
+            fprintf(stderr, "[WARNING]: ");
+            break;
+        case LOG_ERROR:
+            fprintf(stderr, "[ERROR]: ");
+            break;
+    }
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fprintf(stderr, "\n");
+}
+
+static bool is_literal_character_valid(unsigned char c, i32 base)
+{
+    if (base != BASE_HEX)
+    {
+        return c >= '0' && c <= '9' && (c - '0' < base);
+    }
+    if (!(c >= '0' && c <= '9'))
+    {
+        if (!((c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))) return false;
+    }
+    return true;
+}
+
+#define CONVERT_ONE_CHAR(c, result, base) do { \
+    result *= base; \
+    if (c >= 'A' && c <= 'F') result += c - 55; \
+    else if (c >= 'a' && c <= 'f') result += c - 87; \
+    else result += c - '0';                       \
+    } while(0)
+
+bool is_number(u8 c)
+{
+    return c >= '0' && c <= '9';
+}
+
+Bases bases_from_char(char c)
+{
+    if (c == 'x') return BASE_HEX;
+    if (c == 'o') return BASE_OCT;
+    if (c == 'b') return BASE_BIN;
+    if (c == 'd' || is_number(c)) return BASE_DEC;
+    return BASE_INVALID;
+}
+
+i32 sv_to_i32(SV slice, Bases base)
+{
+    i32 result = 0;
+    i32 multiply_by = (i32)base;
+//    u64 i = slice.pointer[0] != '-' ? 3 : 2; // load up the first digit index
+//    if (c == 'b' || c == 'B' && slice.pointer[i-1] == '0') multiply_by = 2;
+//    else if (c == 'o' || c == 'O' && slice.pointer[i-1] == '0') multiply_by = 8;
+//    else if (c == 'x' || c == 'X' && slice.pointer[i-1] == '0') multiply_by = 16;
+//    else if (c == 'd' || c == 'D' && slice.pointer[i-1] == '0') multiply_by = 10;
+//    else
+//    {
+//        multiply_by = 10;
+//        i = slice.pointer[0] != '-' ? 1 : 0;
+//    }
+
+    for (u64 i = 0; i < slice.length;i += 1)
+    {
+        u8 c = slice.pointer[i];\
+        if (c == '_') continue;
+        if (!is_literal_character_valid(c, multiply_by))
+        {
+            aasm_log(LOG_ERROR, "Invalid number literal conversion");
+            result = 0;
+            break;
+        }
+        CONVERT_ONE_CHAR(c, result, multiply_by);
+    }
+    return result;
+}
+
+i32 sv_cmp(SV str1, SV str2)
+{
+	// NOTE: some size considerations are necessery
+    if (str1.length != str2.length) return (i32)((i32)str1.length - (i32)str2.length);
+    for (u64 i = 0;i < str1.length;++i)
+    {
+        if (str1.pointer[i] != str2.pointer[i])
+            return str1.pointer[i] - str2.pointer[i];
+    }
+    return 0;
+}
+
+
+i32 sv_cmp_cstr(SV a, const char* b)
+{
+	size_t blen = strlen(b);
+	if (a.length != blen) return (i32)((i32)a.length - (i32)blen);
+	for (u64 i = 0;i < a.length;++i)
+	{
+		if (a.pointer[i] != b[i])
+			return a.pointer[i] - b[i];
+	}
+	return 0;
+}
+
+
+// ---------------------------------------------------------
+// ------------------------LEXER----------------------------
+// ---------------------------------------------------------
 
 typedef enum Token{
 	LEX_EOF,
